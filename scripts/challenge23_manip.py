c193e177e5b9c2bd1f217adabb65901cfc1ffcdb
Ziang Liu <ziang_liu@outlook.com>
2025-06-25 21:10:43 -0700

    add myochallenge23 myoarm baseline test script

---
+++ scripts/challenge23_manip.py
@@ -0,0 +1,16 @@
+from myosuite.utils import gym
+import myosuite, deprl
+
+env = gym.make("myoChallengeRelocateP1-v0")
+policy = deprl.load_baseline(env)
+
+for ep in range(5):
+    print(f"Episode: {ep} of 5")
+    state = env.reset()[0]
+    while True:
+        action = policy(state)
+        # env.mj_render()
+        next_state, reward, terminated, truncated, info = env.step(action)
+        state = next_state
+        if terminated or truncated:
+            break
