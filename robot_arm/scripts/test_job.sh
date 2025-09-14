da45e412a83c2df0791f0dae1fddc50e787e1b6a
Tailai Talon Ying <tty6@g2-login-02.coecis.cornell.edu>
2025-09-14 15:56:01 -0400

    multirun config for hydra

--- robot_arm/scripts/test_job.sh
+++ robot_arm/scripts/test_job.sh
@@ -1,3 +1,6 @@
 cd /home/tty6/Myosuite-Assistive
 conda activate myosuite
-python -m deprl.main robot_arm/train.yaml
+for i in {1..3}; do
+    SEED=$RANDOM
+    python -m deprl.main robot_arm/train.yaml -m reward.reward_scale=1.0,2.0 tonic.seed=$SEED
+done
